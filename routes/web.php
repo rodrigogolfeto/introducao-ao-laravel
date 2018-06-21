c6b347fd262e3f5eff65f073cf5f7cfcb40bbadb
Rodrigo Golfeto <rgolfeto@gmail.com>
2018-06-21 13:16:45 -0400

    Aula 04 - Configurando Rotas

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contato/{id?}', function($id = null){
+	return "Contato id = ".$id;
+});
+
+Route::post('/contato', function(){
+	dd($_POST);
+	return "Contato POST";
+});
+
+Route::put('/contato', function(){
+	return "Contato PUT";
+});
